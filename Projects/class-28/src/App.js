c83f41f50f375dd6f67146633e40f12267363e4b
Rtxeon <87909941+CodeSuthar@users.noreply.github.com>
2024-09-20 15:07:25 +0530

    class-28

---
+++ Projects/class-28/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>I am function from REACT</h1>
+}
+
+export default App;
---
+++ Projects/class-28/src/App2.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+class App2 extends react.Component {
+    render() {
+        return <h1>Hello, I am Class Component From REACT</h1>;
+    }
+};
+
+export default App2;
