1966651476993ea7c6311eec3a8f7efe48e9eb3f
Rtxeon <87909941+CodeSuthar@users.noreply.github.com>
2024-10-01 14:46:50 +0530

    class 33 & 34

---
+++ Projects/class-34/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const Component = React.Component;
+
+class Student extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Aditya K. Suthar",
+      friend: "Krisha",
+      grade: "X",
+      age: 15
+    };
+  }
+
+  updateName = () => {
+    this.setState({
+      name: "Aditya Suthar"
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Student Details</h1>
+        <p>Name: {this.state.name}</p>
+        <p>Friend: {this.state.friend}</p>
+        <p>Grade: {this.state.grade}</p>
+        <p>Age: {this.state.age}</p>
+        <button onClick={this.updateName}>Update Name</button>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Student />
+  </React.StrictMode>
+);
